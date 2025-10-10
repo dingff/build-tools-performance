1bdf84e227886799a55c6578f016fa4a92612af3
dingff <dingff0923@foxmail.com>
2025-10-10 13:33:27 +0800

    chore: update next 16

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {},
+  experimental: {
+    turbopackFileSystemCacheForDev: true,
+  },
   eslint: {
     ignoreDuringBuilds: true,
   },
