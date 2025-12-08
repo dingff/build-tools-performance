59e60f0f82a5480abbc1d8e66733923acd85922d
dingff <dingff0923@foxmail.com>
2025-12-08 17:02:19 +0800

    feat: update medium

--- src/medium/main.jsx
+++ src/medium/main.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import App0 from './f0'
+import App1 from './f1'
+import App2 from './f2'
+import App3 from './f3'
 import './index.css'
 import { AddAlarmOutlined } from '@material-ui/icons'
 import { IconArrowLeft } from '@tabler/icons-react'
 
-const App1 = React.lazy(() => import('./f1'))
-const App2 = React.lazy(() => import('./f2'))
-const App3 = React.lazy(() => import('./f3'))
 const App4 = React.lazy(() => import('./f4'))
 const App5 = React.lazy(() => import('./f5'))
 const App6 = React.lazy(() => import('./f6'))
@@ -18,13 +18,13 @@ const Main = () => {
     <React.StrictMode>
       <BrowserRouter>
         <App0 />
+        <App1 />
+        <App2 />
+        <App3 />
         <AddAlarmOutlined />
         <IconArrowLeft />
         <React.Suspense fallback={<div>Loading...</div>}>
           <Routes>
-            <Route path="/f1" element={<App1 />} />
-            <Route path="/f2" element={<App2 />} />
-            <Route path="/f3" element={<App3 />} />
             <Route path="/f4" element={<App4 />} />
             <Route path="/f5" element={<App5 />} />
             <Route path="/f6" element={<App6 />} />
